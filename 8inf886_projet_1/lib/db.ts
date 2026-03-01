4869471efed01f2bebfdf121f7f09d0138c7200b
nathan <nathan.toigo@laposte.net>
2026-03-01 10:43:35 -0500

    Fix database port configuration and clean up docker-compose file

--- 8inf886_projet_1/lib/db.ts
+++ 8inf886_projet_1/lib/db.ts
@@ -10,7 +10,7 @@ export const pool =
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: Number(process.env.DB_PORT) || 3306,
+    port: 3306,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
